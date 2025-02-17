b17c5de4dd016b55bcc065b9aa8269d8a0534c00
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2025-02-17 16:51:24 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -18,8 +18,8 @@ class Var(object):
     name = str(getenv('name', 'FileLinkRo_Bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002160257812'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002160257812'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001971825063'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001971825063'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
